c3fde615d6f30b12cc3bdfe91833ef0d686b9d4b
adnanali.aa863 <adnanali.aa863@gmail.com>
2026-09-12 09:41:09 +0000

    performance enhanced and guide added

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -2,59 +2,110 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 1;
+    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 1;
+    private static final int ACCESSIBILITY_SERVICE_REQUEST_CODE = 2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-            launchMainActivity();
+        Button grantPermissionButton = findViewById(R.id.grant_permission_button);
+        grantPermissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAndRequestPermissions();
+            }
+        });
+
+        checkAndRequestPermissions();
+    }
+
+    private void checkAndRequestPermissions() {
+        if (!Settings.canDrawOverlays(this)) {
+            requestOverlayPermission();
+        } else if (!isAccessibilityServiceEnabled()) {
+            requestAccessibilityServicePermission();
         } else {
-            setContentView(R.layout.activity_splash);
-            Button grantPermissionButton = findViewById(R.id.grant_permission_button);
-            grantPermissionButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(SplashActivity.this)) {
-                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                                Uri.parse("package:" + getPackageName()));
-                        startActivityForResult(intent, SYSTEM_ALERT_WINDOW_PERMISSION);
-                    } else {
-                        launchMainActivity();
-                    }
-                }
-            });
+            startMainActivity();
         }
     }
 
+    private void requestOverlayPermission() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE);
+    }
+
+    private void requestAccessibilityServicePermission() {
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivityForResult(intent, ACCESSIBILITY_SERVICE_REQUEST_CODE);
+        Toast.makeText(this, "Please enable the accessibility service for My Application", Toast.LENGTH_LONG).show();
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == SYSTEM_ALERT_WINDOW_PERMISSION) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
-                launchMainActivity();
+        if (requestCode == OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+                checkAndRequestPermissions();
             } else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Overlay permission is required to use the floating ball.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == ACCESSIBILITY_SERVICE_REQUEST_CODE) {
+            if (isAccessibilityServiceEnabled()) {
+                checkAndRequestPermissions();
+            } else {
+                Toast.makeText(this, "Accessibility service is required for smart features.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void launchMainActivity() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
+    private boolean isAccessibilityServiceEnabled() {
+        int accessibilityEnabled = 0;
+        final String service = getPackageName() + "/" + SmartFeatureModule.class.getCanonicalName();
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(
+                    getApplicationContext().getContentResolver(),
+                    android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            // Accessibility is disabled
+        }
+        TextUtils.SimpleStringSplitter colonSplitter = new TextUtils.SimpleStringSplitter(':');
+
+        if (accessibilityEnabled == 1) {
+            String settingValue = Settings.Secure.getString(
+                    getApplicationContext().getContentResolver(),
+                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                colonSplitter.setString(settingValue);
+                while (colonSplitter.hasNext()) {
+                    String accessibilityService = colonSplitter.next();
+                    if (accessibilityService.equalsIgnoreCase(service)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
