07589642ca40c5cbb264e45d4838e37f3ab608ce
adnanali.aa863 <adnanali.aa863@gmail.com>
2026-09-13 05:11:17 +0000

    accesibility crash fix, un

--- app/src/main/java/com/example/myapplication/SmartFeatureModule.java
+++ app/src/main/java/com/example/myapplication/SmartFeatureModule.java
@@ -1,8 +1,6 @@
 package com.example.myapplication;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
-import android.content.Intent;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -53,11 +51,6 @@ public class SmartFeatureModule extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
         instance = this;
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED | AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED;
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
-        info.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS | AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS;
-        setServiceInfo(info);
     }
 
     @Override
